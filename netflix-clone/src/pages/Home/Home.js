1b69bd6785d91f482f5c5da23fcfb3612139a722
gemercy <146848580+gemercy@users.noreply.github.com>
2024-01-31 21:54:24 -0500

    axios and requests

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
-
+import Row
 const Home = () => {
   return (
     <>
